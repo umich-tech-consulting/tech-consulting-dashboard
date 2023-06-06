ba21b9743787a3b46ba1816a40f1a5c3bb59a2e4
bengrnb <66656217+bengrnb@users.noreply.github.com>
2023-06-06 12:03:46 -0400

    Reduce nav link top padding

--- src/components/layout-components/LeftNav.js
+++ src/components/layout-components/LeftNav.js
@@ -49,15 +49,15 @@ const LeftNav = ({isToggleOn, handleToggleClick}) => {
               : "bg-white dark:bg-black"
           }`}
         >
-          <svg
-            className="fill-neutral-9 dark:fill-neutral-4"
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 96 960 960"
-            width="24"
-          >
-            <path d="M730 606v-60h150v60H730Zm50 290-121-90 36-48 121 90-36 48Zm-82-503-36-48 118-89 36 48-118 89ZM210 856V696h-70q-24.75 0-42.375-17.625T80 636V516q0-24.75 17.625-42.375T140 456h180l200-120v480L320 696h-50v160h-60Zm90-280Zm260 134V442q27 24 43.5 58.5T620 576q0 41-16.5 75.5T560 710ZM140 516v120h196l124 74V442l-124 74H140Z" />
-          </svg>
+          {location.pathname === "/announcements" ? (
+            <svg className="fill-neutral-9 dark:fill-neutral-4" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+              <path d="M720-440v-80h160v80H720Zm48 280-128-96 48-64 128 96-48 64Zm-80-480-48-64 128-96 48 64-128 96ZM200-200v-160h-40q-33 0-56.5-23.5T80-440v-80q0-33 23.5-56.5T160-600h160l200-120v480L320-360h-40v160h-80Zm360-146v-268q27 24 43.5 58.5T620-480q0 41-16.5 75.5T560-346Z"/>
+            </svg>
+          ) :(
+            <svg className="fill-neutral-9 dark:fill-neutral-4" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24">
+              <path d="M730 606v-60h150v60H730Zm50 290-121-90 36-48 121 90-36 48Zm-82-503-36-48 118-89 36 48-118 89ZM210 856V696h-70q-24.75 0-42.375-17.625T80 636V516q0-24.75 17.625-42.375T140 456h180l200-120v480L320 696h-50v160h-60Zm90-280Zm260 134V442q27 24 43.5 58.5T620 576q0 41-16.5 75.5T560 710ZM140 516v120h196l124 74V442l-124 74H140Z" />
+            </svg>
+          )}
           <div className={`title-medium text-neutral-9 dark:text-neutral-3 hidden xl:${isToggleOn ? "hidden" : "block"}`}>
             Announcements
           </div>
