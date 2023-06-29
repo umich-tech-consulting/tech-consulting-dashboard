b29cee4a3f16ad9719d7fad40d857f862cbdaaab
bengrnb <66656217+bengrnb@users.noreply.github.com>
2023-06-29 01:30:07 -0400

    Add resources page

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
 import { Helmet } from "react-helmet";
+import ResourcesAccordian from "../components/resources-components-and-data/ResourcesAccordian"
 
+import resourcesData from "../components/resources-components-and-data/resourcesData.json";
 const Resources = () => {
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const handleSelectedItem = (itemId) => {
+    setSelectedItem(itemId === selectedItem ? null : itemId);
+    };
 
     return (
         <>
             <Helmet>
                 <title>Resources</title>
             </Helmet>
-            <div>Resources</div>
+            <div className="w-full flex flex-col p-6">
+                <div className="mb-6 headline-large">Resources</div>
+                <ResourcesAccordian items={resourcesData} selectedItem={selectedItem} handleSelectedItem={handleSelectedItem} />
+            </div>
         </>
     );
   };
