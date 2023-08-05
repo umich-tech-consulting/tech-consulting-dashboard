cbf5c480dd3113852ba5e7c2d0424e4977c719dd
Ben Greenberg <bengrnb@umich.edu>
2023-08-05 19:17:16 -0400

    Add additional asset management cards

--- src/pages/AssetManagement/AssetManagementLanding.js
+++ src/pages/AssetManagement/AssetManagementLanding.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import laptop_return from "../../icons/asset-management/laptop_return.svg"
 import laptop_check_in from "../../icons/asset-management/laptop_check_out.svg"
 import laptop_swap from "../../icons/asset-management/laptop_swap.svg"
+import laptop_lookup from "../../icons/asset-management/laptop_lookup.svg"
+import laptop_warranty from "../../icons/asset-management/laptop_warranty.svg"
+import laptop_quickscan_check_out from "../../icons/asset-management/laptop_quickscan_check_out.svg"
+import laptop_quickscan_return from "../../icons/asset-management/laptop_quickscan_return.svg"
 
 const AssetManagementLanding = () => {
 
@@ -19,12 +23,28 @@ const AssetManagementLanding = () => {
               <div>Return</div>
             </Link>
             <Link className="am-landing-container-button" to="/asset-management/checkout">
-              <img src={laptop_check_in} alt='Laptop Check In Icon'/>
+              <img src={laptop_check_in} alt='Laptop Check Out Icon'/>
               <div>Check Out</div>
             </Link>
             <Link className="am-landing-container-button" to="/asset-management/swap">
               <img src={laptop_swap} alt='Laptop Swap Icon'/>
               <div>Swap</div>
+            </Link>
+            <Link className="am-landing-container-button" to="/asset-management">
+              <img src={laptop_lookup} alt='Laptop Lookup'/>
+              <div>Lookup</div>
+            </Link>
+            <Link className="am-landing-container-button" to="/asset-management">
+              <img src={laptop_warranty} alt='Laptop Warranty Check'/>
+              <div>Warranty</div>
+            </Link>
+            <Link className="am-landing-container-button" to="/asset-management">
+              <img src={laptop_quickscan_check_out} alt='Laptop Quickscan Check Out'/>
+              <div>Quickscan Check Out</div>
+            </Link>
+            <Link className="am-landing-container-button" to="/asset-management">
+              <img src={laptop_quickscan_return} alt='Laptop Quickscan Return'/>
+              <div>Quickscan Return</div>
             </Link>
         </div>
       </div>
