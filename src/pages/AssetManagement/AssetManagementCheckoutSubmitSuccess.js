ccc6dcb1e9808b7335572692a626bd2322d9b855
Ben Greenberg <bengrnb@umich.edu>
2023-09-28 11:46:12 -0400

    Make return and checkout success screens  more responsive

--- src/pages/AssetManagement/AssetManagementCheckoutSubmitSuccess.js
+++ src/pages/AssetManagement/AssetManagementCheckoutSubmitSuccess.js
@@ -13,7 +13,7 @@ const CheckoutSubmitSuccess = () => {
   return (
     <>
       <div className="am-action-success-container fade-in">
-        <div className="flex w-full h-full">
+        <div className="flex w-full h-full flex-wrap md:flex-nowrap">
           <div className="am-action-success-next-steps">
             <div className="am-action-success-header">
               <LottieAnimation animationData={lottie_checkmark} />
@@ -65,7 +65,7 @@ const CheckoutSubmitSuccess = () => {
             </div>
           </div>
           <div className="am-action-success-summary">
-            <div className="title-large mb-12">Summary</div>
+            <div className="title-large mb-6 md:mb-12">Summary</div>
             <div className="am-action-success-summary-info-container">
               <a href={`${tdxBaseUrl}/32/Assets/AssetDet?AssetID=${tdxResponseFromStorage.asset.id}`} target="_blank"
                 rel="noreferrer noopener" className="am-action-success-summary-link">
